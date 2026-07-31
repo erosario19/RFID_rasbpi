f2c551be9e8fc99be556332c647f7c9837982120
erosario19 <elizabethelaine19@gmail.com>
2026-07-31 11:07:29 -0400

    keep display

--- rasp_pi_LCD_test.py
+++ rasp_pi_LCD_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
+from PIL import Image, ImageDraw
 from luma.core.interface.serial import i2c
-from luma.core.render import canvas
 from luma.oled.device import sh1106
 
 # Initialize I2C OLED
@@ -8,10 +8,15 @@ serial = i2c(port=1, address=0x3C)
 oled = sh1106(serial)
 
 def display_message(line1, line2=""):
-    with canvas(oled) as draw:
-        draw.text((0, 0), line1, fill=255)
-        draw.text((0, 20), line2, fill=255)
+  
+    image = Image.new("1", (oled.width, oled.height))
+    draw = ImageDraw.Draw(image)
+
+    draw.text((0, 0), line1, fill=255)
+    draw.text((0, 20), line2, fill=255)
+
+    oled.display(image)
 
 # Test message
-display_message("OLED Test", "Hello Elizabeth!")
+display_message("OLED Test", "test!")
 print("Displayed test message on OLED.")
